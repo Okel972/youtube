cab40781a28ce90b604d127974edf793bb9ff548
Kevin <akmtime@hotmail.com>
2024-07-24 13:00:49 +0200

    first commit

---
+++ melvynx-exercice/src/App.jsx
@@ -0,0 +1,9 @@
+function Button() {
+  return <button className="btn btn-primary flex">Click me</button>;
+}
+
+function App() {
+  return <Button className="btn">test</Button>;
+}
+
+export default App;
